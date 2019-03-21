0f504dc00f34ee08cd84abb5cb05c7a1e25aef7e
HellFlair <48595791+HellFlair@users.noreply.github.com>
2019-03-21 08:17:58 -0700

    imported pong code that I wrote previously

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+     public string Axis = " --- Select system axis for this paddle --- ";
+     public float Speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+// Update is called once per frame
+    void Update()
+    {
+        var v = Input.GetAxisRaw(Axis);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v) * Speed;
+    }
+
+}
